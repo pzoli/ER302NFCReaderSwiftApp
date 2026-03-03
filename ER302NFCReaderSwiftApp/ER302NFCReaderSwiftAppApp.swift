393808747b50a27621194dabd86420734f73f15e
Papp Zoltan <papp.zoltan@infokristaly.hu>
2026-03-03 16:53:36 +0100

    Initial Commit

---
+++ ER302NFCReaderSwiftApp/ER302NFCReaderSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ER302NFCReaderSwiftAppApp.swift
+//  ER302NFCReaderSwiftApp
+//
+//  Created by Papp Zoltán on 2026. 03. 03..
+//
+
+import SwiftUI
+
+@main
+struct ER302NFCReaderSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
